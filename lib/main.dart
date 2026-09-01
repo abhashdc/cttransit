d7f8fbe9f7d5e60fa2fdbebfb916ab8c0c61c18a
Abhash DC <developer.abhash@gmail.com>
2026-09-02 01:49:31 +0545

    update to yellow

--- lib/main.dart
+++ lib/main.dart
@@ -124,8 +124,8 @@ class _TicketPageState extends State<TicketPage> with TickerProviderStateMixin {
   static const double kInnerEnd   = 1.00;
 
   // Colors
-static const Color kOuterColor = Color(0xFFE12A87); // bright magenta / pink ring
-static const Color kRingColor  = Color(0xFFB61D6B); // dark navy / CTtransit blue
+static const Color kOuterColor = Color(0xFFFFB001); // bright magenta / pink ring
+static const Color kRingColor  = Color(0xFFD08E03); // dark navy / CTtransit blue
 
   @override
   void initState() {
